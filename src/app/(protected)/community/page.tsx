a25a79afc5f27eb322031113d6687b814a2fdfcf
mushee22 <mmtmusheer@gmail.com>
2025-07-16 20:58:45 +0530

    fix: correct typo in "Coming Soon" text in community page

--- src/app/(protected)/community/page.tsx
+++ src/app/(protected)/community/page.tsx
@@ -3,7 +3,9 @@ export default function Page() {
   return (
     <>
       <div className=' px-2 space-y-1 '>
-        <p className="h-[90vh] flex flex-col justify-center items-center">Comming Soon</p>
+        <p className="h-[90vh] flex flex-col justify-center items-center">
+          Coming Soon
+        </p>
       </div>
     </>
   )
