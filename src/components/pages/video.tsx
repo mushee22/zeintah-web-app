77d2bed6c7d5e841ca7b59d76e6e005080ea78fa
mushee22 <mmtmusheer@gmail.com>
2026-01-27 15:57:34 +0530

    refactor: Update `disableContextMenu` event parameter type to `MouseEvent` and add `React` to import.

--- src/components/pages/video.tsx
+++ src/components/pages/video.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image'
 import Container from '../elements/container'
 import RelatedVideoSection from '../sections/video/related-video-list'
 import VideoPlayerSection from '../sections/video/video-player'
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 
 export default function VideoPageContent({ chapterId, videoId }: { chapterId?: string, videoId: string }) {
 
     useEffect(() => {
-        const disableContextMenu = (e: PointerEvent) => e.preventDefault();
+        const disableContextMenu = (e: MouseEvent) => {
+            e.preventDefault()
+        };
 
         const disableKeys = (e: KeyboardEvent) => {
             if (
